2e4ffbef3893a2c43904ad5586f6eb252449027a
nikhilkumar202002 <nikhilkumars12343@gmail.com>
2024-01-23 14:58:24 +0530

    ne

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+
+const connect = () => {
+
+  return mongoose.connect("mongodb+srv://user:123@cluster0.jxpil.mongodb.net/GrowGreen?retryWrites=true&w=majority")
+}
+
+
+module.exports = connect;
---
+++ models/seller-model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const SellerSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    place: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    mobile: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: "approved"
+    }
+})
+
+module.exports = mongoose.model("seller", SellerSchema);
